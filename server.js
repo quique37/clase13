4ae7bea0416ec0233451d464456464e365e6aef2
Quique <enrique.jmz37@gmail.com>
2019-06-05 20:46:31 -0500

    2nd commit

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-a
+const fs = require('fs')
+const path = require('path')
+const filepath = path.resolve(__dirname, 'static/index.html')
+
+fs.access(filepath,fs.constants.F_OK, function (err) {
+//function handler (err) {
+//const handler = function (err) {   
+    //C/Users/User/OneDrive/Full-Stack/clase13/static/index.html
+	if (err) {
+        console.log('El archivo no existe')
+        //El archivo no existe
+    } else {
+    console.log('El archivo existe')
+    //El archivo existe´
+    }
+})
+fs.readFile(filepath, function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
